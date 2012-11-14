da10e7bff16e33a16847a9aab8eed91aa401a6c3
Vadim Korolik <vkorolik@gmail.com>
2012-11-14 15:08:08 -0800

    Fixed strange problem with pointers in TKOAutonomous, attempted to fix it in the MarkX Ritwik thing, and failed. We will use what he made and remake it in our own framework (Ritwik Dutta, NEVER RENAME VARIABLES)

--- MarkX_PreCode/Definitions.h
+++ MarkX_PreCode/Definitions.h
@@ -8,6 +8,17 @@
 #include "Vision/RGBImage.h"
 #include "Math.h"
 
+#include "WPILib.h"
+#include "Vision/RGBImage.h"
+#include "Math.h"
+#include <Task.h>
+#include <WPILib.h>
+#include "TKOAutonomous.h"
+#include "TKOConveyor.h"
+#include "TKOIntake.h"
+#include "TKOLogging.h"
+#include "TKORelay.h"
+#include "TKOShooter.h"
 /*!
   \def DSClear()
   Clears the Driver Station LCD of all text.
@@ -22,29 +33,32 @@
 								DriverStationLCD::GetInstance()->UpdateLCD();
 #define gg ;
 
-/*! \mainpage Welcome to TKO 1351's Mark X's Code Documentation!
+
+
+/*! \mainpage Welcome to TKO 1351's MarkIX's Code Documentation!
  *
- * 	This is the Robot Code for the 2013 Season of <TBA>
+ * 	This is the Robot Code for the 2012 Season of Rebound Rumble
  *  
- *  If you haven't already, please visit our <a href="http://www.amhsrobotics.com">website as 
- *  well as our <a href="http://amhsprogramming.blogspot.com">blog.</a>.
+ *  If you haven't already, please visit our <a href="http://www.amhsrobotics.com">website</a>.
  */
-
-
-// Joystick port definition
-const int STICK_1_PORT = 1; // First Joystick port definition  < first drive joystick 
-const int STICK_2_PORT = 2; // Second Joystick port definition < second drive joystick 
-const int STICK_3_PORT = 3; // Third Joystick port definition  < first EVOM joystick 
-const int STICK_4_PORT = 4; // Fourth Joystick port definition < second EVOM joystick 
+//Joystick ports
+bool usingTank;
+const int STICK_1_PORT = 1;
+const int STICK_2_PORT = 2;
+const int STICK_3_PORT = 3;
+const int STICK_4_PORT = 4;
 
 //Jaguar ports
-const int DRIVE_L1_ID = 1; // Define motor port 1 < first left drive motor
-const int DRIVE_L2_ID = 2; // Define motor port 2 < second left drive motor
-const int DRIVE_R1_ID = 3; // Define motor port 4 < first right drive motor
-const int DRIVE_R2_ID = 4; // Define motor port 3 < second right drive motor
-const int EVOM_1_ID = 5;   // Define motor port 5 < first EVOM motor
-const int EVOM_2_ID = 6;   // Define motor port 6 < first EVOM motor
-
+const int DRIVE_L1_ID = 1;
+const int DRIVE_L2_ID = 2;
+const int DRIVE_R1_ID = 3;
+const int DRIVE_R2_ID = 4;
+const int ROLLER_ID = 9;
+const int TURRET_ID = 10;
+const int SPINNER_1_ID = 5;
+const int SPINNER_2_ID = 6;
+const int WRIST_1_ID = 7;
+const int WRIST_2_ID = 8;
 
 // Ports of the various switches
 const int SWITCH_1_PORT = 3;
@@ -53,10 +67,30 @@ const int SWITCH_3_PORT = 1;
 const int LOWER_RELAY_PORT = 1;
 const int UPPER_RELAY_PORT = 2;
 
+// constants for the Intake
+const float WRIST_DOWN = -1.;
+const float WRIST_UP = 1.;
+const float ROLLER_ON = -1.;
+const float ROLLER_OFF = 0.;
+
+// Spinner constants
+const float RADIUS_WHEELS = 4;
+const float TAN_SPINNER_THETA = tan(57.5);
+const float SPINNER_DIV = 4250.0;
+const float HALF_G = .5 * 9.8 * 100. / 2.54;
+const float MID_FENDER = 0.425;
+const float TOP_FENDER = 0.585;
+
 //PID constants
 const float SPINNER_kP = 0.2;		//working P: 0.20, working I: .0034
 const float SPINNER_kI = 0.004;
 const float SPINNER_kD = 0.0;
+const float REVS_PER_METER = 2.51;
+
+//Hoop heights
+const float HOOP_BOT = 23; 
+const float HOOP_TOP = 93;
+const float HOOP_MID = 58;
 
 // Camera Constants
 const int BORDER_SIZE = 3;
@@ -70,8 +104,12 @@ const int SCREEN_WIDTH_PIXELS = 320;
 const int SCREEN_HEIGHT_PIXELS = 240;
 const float TARGET_HEIGHT = 17.;
 const float TARGET_WIDTH = 24.;													// In inches
+const float HOOP_DIAMETER = 18.;
+const float BACKBOARD_TO_HOOP_RIM = 6.;
 
 const float DISTANCE_CONSTANT = .5 * TARGET_HEIGHT * SCREEN_HEIGHT_PIXELS / TANGENT_VERTICAL_CAMERA_ANGLE;
 const float QUARTER_TARGET_WIDTH_SQUARED = .25 * TARGET_WIDTH * TARGET_WIDTH;
+const float BACKBOARD_TO_HOOP_CENTER = BACKBOARD_TO_HOOP_RIM + HOOP_DIAMETER / 2;
+const float BACKBOARD_TO_HOOP_CENTER_SQUARED = BACKBOARD_TO_HOOP_CENTER * BACKBOARD_TO_HOOP_CENTER;
 
 #endif
--- MarkX_PreCode/MarkX.cpp
+++ MarkX_PreCode/MarkX.cpp
@@ -1,5 +1,6 @@
-#include <WPILib.h>
-#include <Task.h>
+//Last edited by Vadim Korolik
+//on 11/13/2012
+
 #include "Definitions.h"
 
 /*
@@ -14,131 +15,260 @@
  TODO: Create other general files
  TODO: Create camera analysis class
 
- */ 
-	
+ */
 
-class MarkX : public SimpleRobot
+class MarkX: public SimpleRobot
 {
-		
-	Joystick stick1, stick2, stick3, stick4;	           // define joysticks
-	CANJaguar drive1, drive2, drive3, drive4, evom1, evom2;// define motors
-	DriverStation *driverStationObj;                  	   // define driver station object
-	Task *autonomousTask;                             	   // define autonomous task
-	Task *operDriveTask;                              	   // define operator task
-	Task *operEvomTask;                               	   // define EVOM task
-	RobotDrive TKODrive;                              	   // define robot drive object
-		
+
+	Joystick stick1, stick2, stick3, stick4; // define joysticks
+	CANJaguar drive1, drive2, drive3, drive4, turret;// define motors
+	DriverStation *driverStationObj; // define driver station object
+	TKOIntake intake;
+	TKOConveyor conveyor;
+	TKOShooter shooter;
+	TKOAutonomous auton;
+	Task *autonomousTask; // define autonomous task
+	Task *operDriveTask; // define operator task
+	Task *operEvomTask; // define EVOM task
+	RobotDrive TKODrive; // define robot drive object
+
 public:
-	MarkX(void): 
-		
-		
-		
-		stick1(STICK_1_PORT),   // initialize joystick 1 < first drive joystick 
-		stick2(STICK_2_PORT),   // initialize joystick 2 < second drive joystick
-		stick3(STICK_3_PORT),   // initialize joystick 3 < first EVOM joystick
-		stick4(STICK_4_PORT),   // initialize joystick 4 < first EVOM joystick
-		
-		drive1(DRIVE_L1_ID),    // initialize motor 1 < first left drive motor
-		drive2(DRIVE_L2_ID),    // initialize motor 2 < second left drive motor
-		drive3(DRIVE_R1_ID),    // initialize motor 3 < first right drive motor
-		drive4(DRIVE_R2_ID),    // initialize motor 4 < second right drive motor
-		evom1(EVOM_1_ID),       // initialize motor 5 < first EVOM motor
-		evom2(EVOM_2_ID),         // initialize motor 5 < first EVOM motor
-		
-		
-		TKODrive(&drive1,&drive2/*Initialize drive system 
-		*/,&drive3,&drive4) 	// already initialized drive motors
+	MarkX(void) :
+
+				stick1(STICK_1_PORT), // initialize joystick 1 < first drive joystick 
+				stick2(STICK_2_PORT), // initialize joystick 2 < second drive joystick
+				stick3(STICK_3_PORT), // initialize joystick 3 < first EVOM joystick
+				stick4(STICK_4_PORT), // initialize joystick 4 < first EVOM joystick
+
+				drive1(DRIVE_L1_ID), // initialize motor 1 < first left drive motor
+				drive2(DRIVE_L2_ID), // initialize motor 2 < second left drive motor
+				drive3(DRIVE_R1_ID), // initialize motor 3 < first right drive motor
+				drive4(DRIVE_R2_ID), // initialize motor 4 < second right drive motor
+				intake(WRIST_1_ID, WRIST_2_ID, ROLLER_ID),
+				conveyor(SWITCH_1_PORT, SWITCH_2_PORT, SWITCH_3_PORT, UPPER_RELAY_PORT,
+						LOWER_RELAY_PORT), turret(TURRET_ID), shooter(SPINNER_1_ID, SPINNER_2_ID),
+				auton(DRIVE_L1_ID, DRIVE_L2_ID, DRIVE_R1_ID, DRIVE_R2_ID),
+
+				TKODrive(&drive1, &drive2/*Initialize drive system 
+				 */, &drive3, &drive4) // already initialized drive motors
+
 	{
-		
+
 		driverStationObj = DriverStation::GetInstance(); // Pulls driver station information 
+
+		TKODrive.SetSafetyEnabled(true);
+		TKODrive.SetInvertedMotor(RobotDrive::kRearLeftMotor, true);
+		TKODrive.SetInvertedMotor(RobotDrive::kFrontRightMotor, true);
+		
 		
 		/* 
 		 Task creation is documented below, and it is necessary to 
 		 run multiple operations simultaneously. 
 		 
 		 taskObject = new Task ( "Name", (FUNCPTR)(Robot::Function)());
-		     ^           ^         ^                    ^
-		| Name of  | Create      |Name of      |Function pointer to       |
-		| the task | task object |task         |function that will be run |
-		  
-		  
+		 ^           ^         ^                    ^
+		 | Name of  | Create      |Name of      |Function pointer to       |
+		 | the task | task object |task         |function that will be run |
+		 
+		 
 		 taskObject->Start(); 
-		     ^
+		 ^
 		 | Starts the task object | 
-		*/
-		
-		
-		autonomousTask = new Task("AutonomousTask", (FUNCPTR)(MarkX::TaskAutonomous)());
+		 */
+
+		autonomousTask = new Task("AutonomousTask", (FUNCPTR) (MarkX::TaskAutonomous)());
 		autonomousTask->Start();
 		// Defines and starts Autonomous movement task
 		// This will run the Autonomous robot movement
-				
-		operDriveTask = new Task("OperatorDriveTask", (FUNCPTR)(MarkX::OperatorDrive)());
+
+		operDriveTask = new Task("OperatorDriveTask", (FUNCPTR) (MarkX::OperatorDrive)());
 		operDriveTask->Start();
 		// Defines and starts Operator movement task
 		// This will allow the operator to move the robot
-		
-		operEvomTask = new Task("OperatorEVOMTask", (FUNCPTR)(MarkX::OperatorEvom)());
+
+		operEvomTask = new Task("OperatorEVOMTask", (FUNCPTR) (MarkX::OperatorEvom)());
 		operEvomTask->Start();
 		// Defines and starts OperatorEVOM Task
 		// This will allow the operator to move the EVOM
 	}
 
-	
-	
 	/*The function name is different because
-	if you keep the default name, building returns an
-	error because the default function type cannot be 
-	overridden, and if their return types are void,
-	building returns another error. 
+	 if you keep the default name, building returns an
+	 error because the default function type cannot be 
+	 overridden, and if their return types are void,
+	 building returns another error. 
 	 */
-		int TaskAutonomous()   
+	int TaskAutonomous()
 	{
-		while (true ) // Loop necessary to make sure that the robot does actually run 
-				{
-					if (driverStationObj->IsAutonomous()) // Making sure the robot doesn't run prematurely
-					{	
-						// Autonomous code goes here
-					}
-				}
-		return 0;
-	}
-	
-	// These are the default called functions, unused because they cannot be overridden.
-	void Autonomous(){}
-	void OperatorControl(){}
-	
-	void arcadeDrive() // Code to drive the robot using the arcade method 
-	{				   // This code was pulled from the MarkIX.cpp file
-		if (stick1.GetTrigger())  // Slow movement
-			TKODrive.ArcadeDrive(stick1.GetY() *.654, stick2.GetX() * .654);
-		if (!stick1.GetTrigger()) // Fast movement
-			TKODrive.ArcadeDrive(stick1.GetY() * .877 , stick2.GetX() * .877);
-	}
-	
-	int OperatorDrive(void) // This code is for the operator control of the 
-	{                       // robot's planar motion
-		TKODrive.SetSafetyEnabled(true);
-		while (IsOperatorControl())
+		while (true) // Loop necessary to make sure that the robot does actually run 
 		{
-			arcadeDrive(); // For now, the drive method is arcade only
+			DSLog(1, "Running Autonomous");
+			if (driverStationObj->IsAutonomous()) // Making sure the robot doesn't run prematurely
+			{
+				auton.initAutonomous();
+				auton.setDrivePID(50, 0.05, .01);
+				auton.setDriveTargetStraight(REVS_PER_METER);
+				auton.startAutonomous();
 
+				while (auton.autonTimer.Get() < 15 && auton.runningAuton)
+				{
+					if (!driverStationObj->IsAutonomous())
+						auton.stopAutonomous(&shooter, &conveyor);
+					auton.autonomousCode(&shooter, &conveyor);
+					Wait(0.005);
+				}
+				auton.stopAutonomous(&shooter, &conveyor);
+			}
 		}
 		return 0;
 	}
-	
+
+	// These are the default called functions, unused because they cannot be overridden.
+	void Autonomous()
+	{
+	}
+	void OperatorControl()
+	{
+	}
+
+	void arcadeDrive() // Code to drive the robot using the arcade method 
+	{ // This code was pulled from the MarkIX.cpp file
+		if (stick1.GetTrigger()) // Slow movement
+			TKODrive.ArcadeDrive(stick1.GetY() * .654, stick2.GetX() * .654);
+		if (!stick1.GetTrigger()) // Fast movement
+			TKODrive.ArcadeDrive(stick1.GetY() * .877, stick2.GetX() * .877);
+	}
+	void tankDrive()
+	{
+		if (!stick1.GetTrigger())
+			TKODrive.TankDrive(stick1.GetY() * .877, stick2.GetY() * .877);
+		if (stick1.GetTrigger())
+			TKODrive.TankDrive(stick1.GetY() * .654, stick2.GetY() * .654);
+	}
+
+	int OperatorDrive(void) // This code is for the operator control of the 
+	{ // robot's planar motion
+		while (driverStationObj->IsOperatorControl())
+		{
+			TKODrive.SetInvertedMotor(RobotDrive::kRearLeftMotor, true);
+			TKODrive.SetInvertedMotor(RobotDrive::kFrontRightMotor, true);
+			conveyor.EndAll();
+			shooter.DecreaseSpeed(250);
+			usingTank = true;
+
+			DSLog(5, "Position: %f ", auton.getPosition(1));
+			Operator();
+
+			Wait(.005);
+		}
+		return 0;
+	}
+
 	int OperatorEvom(void) // This code is for the operator control of the
-	{					   // robot's EVOM apparatus
+	{ // robot's EVOM apparatus
 		while (true)
 		{
-			// Evom code goes here.
+			int counter = 0;
+			float average = 0;
+			float total = 0;
+			if (counter % 30 == 0)
+			{
+				average = total / 30;
+				total = 0;
+				DSLog(4, "Actual: %f", shooter.GetSpeed() );
+			}
+			total += shooter.GetSpeed();
+			DSLog(3, "Spinner : %f", average);
+			DSLog(1, "Number of balls: %d", conveyor.GetNumBalls());
+			DSLog(2, "Setpoint: %f", shooter.GetSetpoint());
+
+			counter++;
 		}
 		return 0;
+	}
+
+	void Operator()
+	{
+		//BEGIN DRIVE
+		if (usingTank == true)
+			tankDrive();
+		if (!usingTank)
+			arcadeDrive();
+
+		//BEGIN EVOM
+		if (stick3.GetRawButton(5) && !stick3.GetRawButton(3))
+		{
+			conveyor.OverrideAll(); // Currently uses a DSLog(2)
+		}
+		if (!stick3.GetRawButton(5) && stick3.GetRawButton(3))
+		{
+			conveyor.Run(shooter.IsUpToSpeed());
+		}
+		else if (stick3.GetRawButton(4))
+			conveyor.Reverse();
+
+		if (!stick3.GetRawButton(3) && !stick3.GetRawButton(5) && !stick3.GetRawButton(4))
+		{
+			conveyor.EndAll();
+		}
+		if (stick3.GetRawButton(2) && shooter.ReadyToFire())
+		{
+			conveyor.OverrideAll();
+		}
+		if (stick3.GetTrigger())
+		{
+			intake.RollerMove(ROLLER_ON);
+			conveyor.Run(shooter.IsUpToSpeed());
+		}
+		else
+			intake.RollerMove(ROLLER_OFF);
+		intake.WristMove(stick3.GetY());
+		turret.Set(-stick4.GetX() * .350);
+
+		if (stick2.GetRawButton(8))
+		{
+			DSLog(5, "Average: %f", shooter.AverageSpeed());
+		}
+		else
+			shooter.ClearAverage();
+
+		if (stick4.GetRawButton(3))
+		{
+			shooter.IncreaseSpeed(4250 * driverStationObj->GetAnalogIn(4));
+			shooter.Shoot();
+		}
+		else if (stick4.GetRawButton(4))
+		{
+			shooter.IncreaseSpeed(4250 * driverStationObj->GetAnalogIn(3));
+			shooter.Shoot();
+		}
+		else if (stick4.GetRawButton(5))
+		{
+			shooter.IncreaseSpeed(4250 * driverStationObj->GetAnalogIn(2));
+			shooter.Shoot();
+		}
+		else if (stick2.GetRawButton(7))
+		{
+			usingTank = false;
+		}
+		else if (stick2.GetRawButton(6))
+		{
+			usingTank = true;
+		}
+		else
+		{
+			shooter.DecreaseSpeed(250);
+		}
+		if (stick4.GetTrigger())
+		{
+			shooter.DecreaseSpeed(250);
+		}
 	}
 };
 
-START_ROBOT_CLASS(MarkX); // Starts running the Robot Class
+START_ROBOT_CLASS(MarkX)
+; // Starts running the Robot Class
 
 /* Code written by Ritwik Dutta, Daniel Braginsky, and Vadim Korolik, with
-   help from Matt Pleva
-*/
+ help from Matt Pleva
+ */
